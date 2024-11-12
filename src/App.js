a9fcca98c68b83251d9731eb477b332fbab78abb
dissentty <dissentty@gmail.com>
2024-11-12 18:12:50 +0300

    update balance

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import React, {useEffect, useState} from 'react';
 function App() {
   const [counter, setCounter] = React.useState(0);
   const [income, setIncome] = React.useState(0);
+  const [costMine, setCostMine] = React.useState(50);
 
   const click = () => {
     setCounter(counter + 1);
   }
   
   const buy_mine = () => {
-    if (counter >= 50) {
-      setIncome(income + 1);
-      setCounter(counter - 50);
+    if (counter >= costMine) {
+      setIncome((prevValue) => prevValue + 1);
+      setCounter((prevValue) => prevValue - costMine);
+      setCostMine((prevValue) => prevValue * 1.1);
     }
   }
 
@@ -37,7 +39,7 @@ function App() {
       <header className="App-header">
         <aside>
           <div>Shop</div>
-          <p>Buy mine (-50 points, +1 income)</p>
+          <p>Buy mine (-{costMine.toFixed(1)} points, +1 income)</p>
           <button onClick={buy_mine}>buy mine</button>
           <p>Buy pub (-500 points, +12 income)</p>
           <button onClick={buy_pub}>buy pub</button>
@@ -45,7 +47,7 @@ function App() {
         <aside className="leftPad">
         passive income: {income}
         </aside>
-        <div>{counter}</div>
+        <div>{counter.toFixed(1)}</div>
           <button onClick={click}>
             click
           </button>
