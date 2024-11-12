5ab13eec76310f9158945959aadbd0ffedd489a9
dissentty <dissentty@gmail.com>
2024-11-12 17:43:06 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  const [counter, setCounter] = React.useState(0);
+  const [income, setIncome] = React.useState(0);
+
+  const click = () => {
+    setCounter(counter + 1);
+  }
+  
+  const buy_mine = () => {
+    if (counter >= 50) {
+      setIncome(income + 1);
+      setCounter(counter - 50);
+    }
+  }
+
+  const buy_pub = () => {
+    if (counter >= 500) {
+      setIncome(income + 12);
+      setCounter(counter - 500);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevValue) => prevValue + income); // увеличиваем значение
+    }, 1000); // интервал в 1 секунду
+
+    // Очистка интервала при размонтировании компонента
+    return () => clearInterval(interval);
+  }, [income]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <aside>
+          <div>Shop</div>
+          <p>Buy mine (-50 points, +1 income)</p>
+          <button onClick={buy_mine}>buy mine</button>
+          <p>Buy pub (-500 points, +12 income)</p>
+          <button onClick={buy_pub}>buy pub</button>
+        </aside>
+        <aside className="leftPad">
+        passive income: {income}
+        </aside>
+        <div>{counter}</div>
+          <button onClick={click}>
+            click
+          </button>
       </header>
     </div>
   );
